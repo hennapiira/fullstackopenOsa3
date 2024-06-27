19c3f2e60fdd971e938d4c04d5c79181ea5d8c7e
Henna Piira <AB8308@student.jamk.fi>
2024-06-27 17:01:41 +0300

    yhdistäminen frontendiin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
 
@@ -9,6 +10,7 @@ morgan.token('postData', (req, res) => {
 });
 
 app.use(morgan(':method :url :status :response-time ms - :postData'));
+app.use(cors());
 
 let persons = [
   {
@@ -97,6 +99,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
