1c23b147d03497e870d393754dff3fef2d87fcb6
Henna Piira <AB8308@student.jamk.fi>
2024-06-28 12:05:59 +0300

    lisätty lint

--- models/person.js
+++ models/person.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
   .then((result) => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -20,6 +20,6 @@ const personSchema = new mongoose.Schema({
     required: true,
   },
   number: { type: String, required: true },
-});
+})
 
-module.exports = new mongoose.model('Person', personSchema);
+module.exports = new mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,57 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.4lczcyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.4lczcyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const addPerson = (name, number) => {
   const person = new Person({
     name: name,
     number: number,
-  });
+  })
 
   person.save().then(() => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
-};
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
 
 const listPeople = () => {
   Person.find({}).then((result) => {
-    console.log('phonebook:');
+    console.log('phonebook:')
     result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-      mongoose.connection.close();
-    });
-  });
-};
+      console.log(`${person.name} ${person.number}`)
+      mongoose.connection.close()
+    })
+  })
+}
 
-const argsLength = process.argv.length;
+const argsLength = process.argv.length
 if (argsLength === 3) {
-  listPeople();
+  listPeople()
 } else if (argsLength === 5) {
-  const name = process.argv[3];
-  const number = process.argv[4];
-  addPerson(name, number);
+  const name = process.argv[3]
+  const number = process.argv[4]
+  addPerson(name, number)
 } else {
   console.log(
     'Please provide the correct number of arguments: node mongo.js <password> <name> <number>'
-  );
-  mongoose.connection.close();
-  process.exit(1);
+  )
+  mongoose.connection.close()
+  process.exit(1)
 }
