838dc61592ebb748639559915d7f4d6ddfd4eb41
Henna Piira <AB8308@student.jamk.fi>
2024-06-28 12:24:28 +0300

    tehty tehtävä 3.20

--- models/person.js
+++ models/person.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 mongoose
   .connect(url)
   .then((result) => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: { type: String, required: true },
-})
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{7,}/.test(v); // Validointi puhelinnumeron muodon perusteella
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
+});
 
-module.exports = new mongoose.model('Person', personSchema)
+module.exports = new mongoose.model('Person', personSchema);
